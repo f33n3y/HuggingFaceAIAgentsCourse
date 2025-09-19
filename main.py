682d020f9885af140f68a27ffae8f7680c9896fc
f33n3y <f33n3y_a@hotmail.co.uk>
2025-09-19 10:16:24 +0100

    Use local qwen2 model

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 # Unit 2.1 The smolagents framework
-from smolagents import CodeAgent, DuckDuckGoSearchTool, InferenceClientModel, tool
+from smolagents import (CodeAgent, DuckDuckGoSearchTool, InferenceClientModel, tool, Tool, VisitWebpageTool,
+                        FinalAnswerTool, ToolCallingAgent, LiteLLMModel)
+from langfuse import get_client
+from openinference.instrumentation.smolagents import SmolagentsInstrumentor
+
+# Langfuse Telemetry
+langfuse = get_client()
+if langfuse.auth_check():
+    print("Langfuse client is authenticated and ready!")
+else:
+    print("Authentication failed. Please check your credentials and host.")
+
+SmolagentsInstrumentor().instrument()
+
+# Local model
+model = LiteLLMModel(
+    model_id="ollama_chat/qwen2:7b",
+    api_base="http://127.0.0.1:11434",
+    num_ctx=8192,
+)
 
 # Finding a party playlist using DuckDuckGo
-#agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=InferenceClientModel())
+#agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=model)
 #agent.run("Search for the best music recommendations for a Mr Robot themed party")
 
-# Tool to suggest a menu based on the occasion
+# Suggest menu based on the occasion
+
 @tool
 def suggest_menu(occasion: str) -> str:
     """
     Suggests a menu based on the occasion.
     Args:
-        occasion (str): The type of occasion for the party. Allowed values are:
-                        - "casual": Menu for casual party.
-                        - "formal": Menu for formal party.
-                        - "superhero": Menu for superhero party.
-                        - "custom": Custom menu.
+        occasion: The type of occasion for the party.
     """
     if occasion == "casual":
         return "Pizza, snacks, and drinks."
@@ -26,12 +42,31 @@ def suggest_menu(occasion: str) -> str:
     else:
         return "Custom menu for the butler."
 
+#agent = CodeAgent(tools=[suggest_menu], model=model)
+#agent.run("Prepare a formal menu for the party.")
 
-# Alfred, the butler, preparing the menu for the party
-agent = CodeAgent(tools=[suggest_menu], model=InferenceClientModel())
+
+#agent = CodeAgent(tools=[], model=InferenceClientModel(), additional_authorized_imports=['datetime'])
 
 # Preparing the menu for the party
-agent.run("Prepare a formal menu for the party.")
+#agent.run("Prepare a formal menu for the party.")
 
+# Work out party preparation time
+# agent.run(
+#     """
+#     Alfred needs to prepare for the party. Here are the tasks:
+#     1. Prepare the drinks - 30 minutes
+#     2. Decorate the mansion - 60 minutes
+#     3. Set up the menu - 45 minutes
+#     4. Prepare the music and playlist - 45 minutes
+#
+#     If we start right now, at what time will the party be ready?
+#     """
+# )
+
+# Using a ToolCallingAgent (generates JSON to call tools)
+agent = ToolCallingAgent(tools=[DuckDuckGoSearchTool()], model=model)
+
+agent.run("What was the score in the last game of football played by Celtic Football Club?")
 
 
